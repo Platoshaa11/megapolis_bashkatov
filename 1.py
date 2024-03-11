720b1b32bb4f96971f876c1729b1eb1a82ccd6d4
Platoshaa11 <platonbashkatov@gmail.com>
2024-03-11 17:56:06 +0300

    1.1

--- 1.py
+++ 1.py
@@ -1,8 +1,16 @@
-fin=open('products.csv', 'r', encoding='utf-8') '''открываю данный файл на чтение'''
-fout=open('products_new.csv', 'w', encoding='utf-8') '''открываю созданный файл для записи'''
+'''открываю данный файл на чтение'''
+fin=open('products.csv', 'r', encoding='utf-8')
+'''открываю созданный файл для записи'''
+fout=open('products_new.csv', 'w', encoding='utf-8')
 
 otv_sum=0
-print(fin.readline().strip(), 'total', sep=';', file=fout) '''записываю названия столбцов в новую таблицу'''
+'''записываю названия столбцов в новую таблицу'''
+print(fin.readline().strip(), 'total', sep=';', file=fout)
 for s in fin:
+    '''считываю данные из таблицы'''
     cat, prod, date, price, unit=s.strip().split(';')
-    print(cat, prod, date, price, unit, float(price)*float(unit), sep=';', file=fout) '''записываю данные из старой таблицы + искомую цену в новую таблицу'''
+    '''записываю данные из старой таблицы + искомую цену в новую таблицу'''
+    print(cat, prod, date, price, unit, float(price)*float(unit), sep=';', file=fout)
+    '''добавляю итоговую цену в сумму для ответа'''
+    otv_sum+=float(price)*float(unit)
+print(otv_sum)
